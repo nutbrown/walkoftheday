1056cb384c4262483e12a5e4dcdb9b37f6f99d2a
nutbrown <davidpoplar0531@gmail.com>
2023-05-21 16:47:00 +0900

    [BE]feat: DTO 설정

--- server/osan/src/main/java/com/ssafy/osan/domain/review/dto/Review.java
+++ server/osan/src/main/java/com/ssafy/osan/domain/review/dto/Review.java
@@ -1,7 +1,96 @@
 package com.ssafy.osan.domain.review.dto;
 
+import java.util.Date;
+
 public class Review {
         private int reviewId;
+        private int category;
+        private int routeId;
+        private int spotId;
         private String writer;
         private int rating;
+        private Date createdAt;
+        private Date updatedAt;
+
+        public Review() {
+        }
+
+        public int getReviewId() {
+                return reviewId;
+        }
+
+        public void setReviewId(int reviewId) {
+                this.reviewId = reviewId;
+        }
+
+        public int getCategory() {
+                return category;
+        }
+
+        public void setCategory(int category) {
+                this.category = category;
+        }
+
+        public int getRouteId() {
+                return routeId;
+        }
+
+        public void setRouteId(int routeId) {
+                this.routeId = routeId;
+        }
+
+        public int getSpotId() {
+                return spotId;
+        }
+
+        public void setSpotId(int spotId) {
+                this.spotId = spotId;
+        }
+
+        public String getWriter() {
+                return writer;
+        }
+
+        public void setWriter(String writer) {
+                this.writer = writer;
+        }
+
+        public int getRating() {
+                return rating;
+        }
+
+        public void setRating(int rating) {
+                this.rating = rating;
+        }
+
+        public Date getCreatedAt() {
+                return createdAt;
+        }
+
+        public void setCreatedAt(Date createdAt) {
+                this.createdAt = createdAt;
+        }
+
+        public Date getUpdatedAt() {
+                return updatedAt;
+        }
+
+        public void setUpdatedAt(Date updatedAt) {
+                this.updatedAt = updatedAt;
+        }
+
+        @Override
+        public String toString() {
+                return "Review{" +
+                        "reviewId=" + reviewId +
+                        ", category=" + category +
+                        ", routeId=" + routeId +
+                        ", spotId=" + spotId +
+                        ", writer='" + writer + '\'' +
+                        ", rating=" + rating +
+                        ", createdAt=" + createdAt +
+                        ", updatedAt=" + updatedAt +
+                        '}';
+        }
+
 }
--- server/osan/src/main/java/com/ssafy/osan/domain/user/dto/User.java
+++ server/osan/src/main/java/com/ssafy/osan/domain/user/dto/User.java
@@ -1,4 +1,53 @@
 package com.ssafy.osan.domain.user.dto;
 
 public class User {
+    private String userId;
+    private String password;
+    private String email;
+    private String nickname;
+
+    public User() {
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId='" + userId + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", nickname='" + nickname + '\'' +
+                '}';
+    }
 }
