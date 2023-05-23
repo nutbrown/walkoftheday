b7dc40f9c59d7ccc22705d3e145deb28da0d9deb
hejcj <bambiju156@gmail.com>
2023-05-23 13:50:52 +0900

    [FE]프론트 공통 컴포넌트 개발

--- client/osan/src/components/common/CommonContent.vue
+++ client/osan/src/components/common/CommonContent.vue
@@ -1,1 +1,25 @@
 // 9. 디테일 설명 출력
+<template>
+  <div>
+    <div>산책 한 마디</div>
+     <p>
+        {{content}}
+     </p>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "CommentContent",
+    props:{
+        content: {
+            type: String,
+            required : true
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
